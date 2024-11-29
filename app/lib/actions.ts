decfa87b1746b848d55123e26779d66c7bce10c1
Vladislav Didenko <hater.developer.17@gmail.com>
2024-11-29 21:43:34 +0100

    Fixed eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,7 +59,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
             INSERT INTO invoices (customer_id, amount, status, date)
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
-    } catch(_) {
+    } catch {
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -94,7 +94,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
             SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
             WHERE id = ${id}
         `;
-    } catch (_) {
+    } catch {
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
     
@@ -107,7 +107,7 @@ export async function deleteInvoice(id: string) {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         expirePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
-    } catch(_) {
+    } catch {
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 };
